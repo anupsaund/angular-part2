9a64362443f0618e6acdc45f57eccad93b57c67f
Anup Saund <anup.saund@gmail.com>
2021-02-16 12:01:46 -0800

    initial commit

---
+++ sandbox/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchMovieComponent } from './search-movie/search-movie.component';
+
+const routes: Routes = [{
+    path: '',
+    component: SearchMovieComponent
+  } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ sandbox/src/app/search-movie/search-movie.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+
+
+@Component({
+  selector: 'app-search-movie',
+  templateUrl: './search-movie.component.html',
+  styleUrls: ['./search-movie.component.sass']
+})
+export class SearchMovieComponent implements OnInit {
+  todos: any;
+  inputHighlight: string =  'red';
+  movies: any;
+
+  constructor(private movieAPI: MovieService) {
+
+    this.todos = [
+      {id: 1, content: 'eat'},
+      {id: 2, content: 'sleep'},
+    ]
+
+   }
+
+  searchMovie(){
+    let searchTerm = 'shrek'; // Get that from an input control.
+    this.movieAPI.getMovies(searchTerm)
+    .subscribe(
+      // This is our callback when the getMovies returns some data
+      (data:any) =>{
+        this.movies = data['Search']; // data.Search;
+    })
+  }
+
+  changeBorder(e: any, color: string){
+    console.log(e.target)
+    e.target.style.borderColor = color;
+    console.log(color)
+  }
+
+  onKeyUp(event: any, data: string) {
+    console.log( `Key Up: ${event.target.value} Data: ${data}`);
+  }
+ 
+  onKeyDown(event: any) {
+    console.log( `Key Down: ${event.target.value}`);
+  }
+ 
+  onKeyPress(event: any) {
+    console.log( `Key Press: ${event.target.value}`);
+  } 
+
+  ngOnInit(): void {
+    this.inputHighlight = 'red'
+  }
+
+}
